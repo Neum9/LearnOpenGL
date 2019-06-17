57a1e4f93fe56c286089c45c2c2d3c45d583aa8a
Neum <1975584042@qq.com>
2019-06-17 23:51:45 +0800

    working translation 2019 6 17

--- TransitionTest/main.cpp
+++ TransitionTest/main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
-#include<glm\glm.hpp>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+using namespace glm;
+using namespace std;
+
 
 int main() {
+
+	vec4 vec(1.0F, 0.0F, 0.0F, 1.0F);
+
+	mat4 trans = mat4(1.0F);
+
+	vec = trans * vec;
+
+	trans = translate(trans, vec3(1.0F, 1.0F, 0.0F));
+
+	vec = trans * vec;
+
+	cout << vec.x << vec.y << vec.z << endl;
+
+	system("pause");
 	return 0;
 }
