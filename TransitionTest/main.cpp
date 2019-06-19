92cea4cf61210bb0054fc41ed904836ee65883ac
Cjc <1975584042@qq.com>
2019-06-19 16:51:03 +0800

    working translation 2019 6 19 16:50

--- TransitionTest/main.cpp
+++ TransitionTest/main.cpp
@@ -8,19 +8,27 @@ using namespace std;
 
 
 int main() {
+#pragma region DataTest
 
-	vec4 vec(1.0F, 0.0F, 0.0F, 1.0F);
 
-	mat4 trans = mat4(1.0F);
+	//vec4 vec(1.0F, 0.0F, 0.0F, 1.0F);
 
-	//获得位移矩阵
-	trans = translate(trans, vec3(1.0F, 1.0F, 0.0F));
+	//mat4 trans = mat4(1.0F);
 
-	vec = trans * vec;
+	////获得位移矩阵
+	//trans = translate(trans, vec3(1.0F, 1.0F, 0.0F));
 
-	//结果为(2,1,0)
-	cout << vec.x << vec.y << vec.z << endl;
+	//vec = trans * vec;
 
-	system("pause");
+	////结果为(2,1,0)
+	//cout << vec.x << vec.y << vec.z << endl;
+
+	//system("pause");
+#pragma endregion
+
+	mat4 model = mat4(1.0F);
+
+	model = rotate(model, radians(-55.0F), vec3(1.0F, 0.0F, 0.0F));
+
 	return 0;
 }
