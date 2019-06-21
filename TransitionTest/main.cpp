5b63b0a7236ea9029c8abc4991e63692d26bef05
Cjc <1975584042@qq.com>
2019-06-21 09:24:55 +0800

    woking 3d 2019 6 21 9:24

--- TransitionTest/main.cpp
+++ TransitionTest/main.cpp
@@ -34,6 +34,16 @@ mat4 model = mat4(1.0F);
 mat4 view = mat4(1.0F);
 mat4 projection = mat4(1.0F);
 
+#pragma region cube
+
+unsigned int VAO_CUBE;
+unsigned int VBO_CUBE;
+unsigned int EBO_CUBE;
+
+#pragma endregion
+
+
+
 int main() {
 #pragma region DataTest
 
@@ -211,6 +221,40 @@ void DrawCube() {
 		-0.5F,-0.5F,-0.5F,	0.0F,0.0F,
 		-0.5F,0.5F,-0.5F,	0.0F,1.0F,
 	};
+
+	unsigned int indexes[] = {
+		0,1,2,
+		2,3,0,
+		4,5,1,
+		1,0,4,
+		4,0,3,
+		3,7,4,
+		3,2,6,
+		6,7,3,
+		1,5,6,
+		6,2,1,
+		7,6,5,
+		5,4,7
+	};
+
+	glGenVertexArrays(1, &VAO_CUBE);
+	glGenBuffers(1, &VBO_CUBE);
+	glGenBuffers(1, &EBO_CUBE);
+
+	glBindVertexArray(VAO_CUBE);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO_CUBE);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO_CUBE);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indexes), indexes, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)3);
+	glEnableVertexAttribArray(1);
+
 }
 
 //‰÷»æ
