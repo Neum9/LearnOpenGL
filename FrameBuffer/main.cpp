9a7a8602f129a5e07f65864d26219f37064aad4e
Cjc <1975584042@qq.com>
2019-07-12 18:15:20 +0800

    frameBuffer

--- FrameBuffer/main.cpp
+++ FrameBuffer/main.cpp
@@ -230,6 +230,8 @@ void TextureEnclosure() {
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 800, 600, 0, GL_RGB, GL_UNSIGNED_INT, NULL);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, texture, 0);
 }
 
 void Renderer() {
